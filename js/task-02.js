b826d648cf4340b9fa9d3da21331407823372e5b
Maria Naumova <naumovamawa@gmail.com>
2022-12-03 17:38:42 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -5,4 +5,21 @@ const ingredients = [
   'Tomatos',
   'Herbs',
   'Condiments',
-];
+
+   // Напиши скрипт, который для каждого элемента массива ingredients:
+].map(ingredient => {
+
+  // Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+  const li = document.createElement('li');
+
+  // Добавит название ингредиента как его текстовое содержимое.
+  // Добавит элементу класс item.
+  li.textContent = ingredient;
+  li.class = 'item';
+  return li;
+});
+  
+  // После чего вставит все <li> за одну операцию в список ul#ingredients.
+  const ul = document.getElementById('ingredients');
+  console.log(ingredients);
+  ul.prepend(...ingredients);
